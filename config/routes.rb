639688c753ba06ae59d9807eafc2bd4de8e076e0
EC2 Default User <ec2-user@ip-172-31-29-109.us-east-2.compute.internal>
2020-06-29 14:29:17 +0000

    add login and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get "login"=>"users#login_form"
+  post "login"=>"users#login"
+  post "logout"=>"users#logout"
+  get "/signup"=>"users#new"
+  post "users/create"=>"users#create"
+  get 'users/index'=>"users#index"
+  get "users/:id"=>"users#show"
+  get "users/:id/edit"=>"users#edit"
+  post "users/:id/update"=>"users#update"
+  
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id"=> "posts#show"
@@ -6,6 +16,7 @@ Rails.application.routes.draw do
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
+  
   get "/" => "home#top"
   get "/about" => "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
